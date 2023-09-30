08decb4c25137990190071bd150228685b8bfbf6
jinbridge <2635480475@qq.com>
2023-10-01 00:44:30 +0800

    feat: import opencv in vcpkg

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,6 @@ int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
-//    QFontDatabase::addApplicationFont("qrc:/res/font/materialdesignicons-webfont.ttf");
     engine.load(QUrl(QStringLiteral("qrc:/qml/main.qml")));
 
     if (engine.rootObjects().isEmpty())
