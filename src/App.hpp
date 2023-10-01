9a4d66dea18840c920806d22057ca3d4adb9c874
jinbridge <2635480475@qq.com>
2023-10-02 00:46:16 +0800

    feat: remove preload img

--- src/App.hpp
+++ src/App.hpp
@@ -15,7 +15,6 @@ class App: public QQuickView {
     Q_OBJECT
 public:
     App(QWindow *parent = 0): QQuickView(parent), _imgCore(new Core()) {
-        _imgCore->loadImg("C:\\Users\\26354\\Desktop\\avatar.png");
         engine()->addImageProvider("imgprovider", new ImgProvider(_imgCore));
         setSource(QUrl("qrc:/qml/main.qml"));
         setResizeMode(QQuickView::SizeRootObjectToView);
